70a449d340d7cbfc74c9ae1dfe632147bcf28c77
deepika9121889 <dipikalawale05@gmail>
2026-03-03 17:04:27 +0530

    second program

---
+++ TNS/src/com/tnsdemo/samp.java
@@ -0,0 +1,17 @@
+package com.tnsdemo;
+
+public class samp {
+	int a=10;
+	String s="Java";
+	public void show()
+	{
+		System.out.println("The integer value is :"+a);
+		System.out.println("The string value is :"+s);
+	}
+	public static void main(String[] args) {
+		samp obj = new samp();
+		obj.show();
+		
+	}
+
+}
