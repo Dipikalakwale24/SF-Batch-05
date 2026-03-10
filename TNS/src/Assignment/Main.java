c7848bb5bf934d4c762b3a19a49a9d9d64ca911f
deepika9121889 <dipikalawale05@gmail>
2026-03-10 17:20:11 +0530

    Assignment Solution

---
+++ TNS/src/Assignment/Main.java
@@ -0,0 +1,26 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        StringBuilder lower = new StringBuilder();
+        StringBuilder upper = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(Character.isUpperCase(ch)) {
+                upper.append(ch);
+            } else {
+                lower.append(ch);
+            }
+        }
+
+        System.out.println(lower.toString().trim() + upper.toString());
+    }
+}
